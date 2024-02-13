60984723846385e8b596077b68777d938d4d88f1
Dawid Mostowski <dawid-mostowski@o2.pl>
2024-02-13 13:04:26 +0100

    Add files via upload

---
+++ js/common.js
@@ -0,0 +1,8 @@
+// common.js
+function hideAllContainers() {
+    document.getElementById("searchBooks").style.display = "none";
+    document.getElementById("searchGuests").style.display = "none";
+    document.getElementById("contactInfo").style.display = "none";
+    document.getElementById("adminPanel").style.display = "none";
+    // Dodaj kolejne linie w razie dodania nowych kontenerów
+}
