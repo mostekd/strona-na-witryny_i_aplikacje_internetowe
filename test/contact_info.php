3fa7c4364f576139ee705f1d0670989a1e40e20a
Dawid Mostowski <dawid-mostowski@o2.pl>
2024-02-27 12:57:36 +0100

    Delete test directory

--- test/contact_info.php
+++
@@ -1,7 +0,0 @@
-<!-- contact_info.php -->
-<div id="contactInfo" class="contact-container">
-    <h3>Informacje Kontaktowe</h3>
-    <p>Adres: ul. Szkolna 1 , 54-230 Gdańsk</p>
-    <p>Telefon: 123-456-789</p>
-    <p>Email: biblioteka@wesolaszkola.pl</p>
-</div>
