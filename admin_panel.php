e5f99aac6a12f7d691950fd610983dd878ab4403
Dawid Mostowski <dawid-mostowski@o2.pl>
2024-02-28 22:40:11 +0100

    update

--- admin_panel.php
+++ admin_panel.php
@@ -17,7 +17,31 @@ if (!isset($_SESSION["admin_logged_in"]) || $_SESSION["admin_logged_in"] !== tru
     <link rel="stylesheet" href="admin_panel.css">
 </head>
 <body>
-    <h2>Biblioteka Wesoła Szkoła<br>Panel Administracyjny</h2>
-    <a href="login.php" id="logout">Wyloguj się</a>
+    <header>
+        <h2>Biblioteka Wesoła Szkoła<br>Panel Administracyjny</h2>
+        <a href="login.php" id="logout">Wyloguj się</a>
+    </header>
+    <?php
+    $host = "localhost"; // Host bazy danych
+    $dbname = "wiai"; // Nazwa bazy danych
+    $username = "root"; // Nazwa użytkownika bazy danych
+    $password = ""; // Hasło użytkownika bazy danych
+
+    $connect = mysqli_connect($host, $username, $password, $dbname);
+    if(!$connect)
+    {
+        die("Connection failed: " . mysqli_connect_error());
+    }
+    $query = 'SELECT `artykul_id`, `title`, `tresc`, `link` FROM `artykul` WHERE 1';
+    $data = mysqli_query($connect, $query);
+    if (mysqli_num_rows($data) > 0)
+    {
+        while($row = mysqli_fetch_assoc($data))
+        {
+            echo "<div id='wpis' class='artykul'><a href='artykul_admin.php?id=".$row['artykul_id']."'>".$row['title']."</a><article>".substr($row['tresc'],0,150)." ...</article></div>";
+        }
+    }
+    mysqli_close($connect);
+?>
 </body>
 </html>
---
+++ artykul_admin.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="pl">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Biblioteka</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <div class="header-container">
+        <h2>Biblioteka Wesoła Szkoła</h2>
+    </div>
+<?php
+		$article_id = $_GET['id'];
+		
+		$connect = mysqli_connect("localhost","root","","wiai");
+		if (!$connect) 
+		{
+			die("Connection failed: " . mysqli_connect_error());
+		}
+		
+		$query = "SELECT `artykul_id`, `title`, `tresc`, `link` FROM `artykul` WHERE artykul_id =".$article_id;
+		$data = mysqli_query($connect, $query);
+		
+		if (mysqli_num_rows($data) > 0) 
+		{
+			while($row = mysqli_fetch_assoc($data))
+			{
+				echo "<div class='artykul_full'>".$row['title']."<article>".$row['tresc']."</article></div>";
+			}
+		}	
+
+		mysqli_close($connect);
+		?>
+    <a href="./index.php"><button>Powrót</button></a>
+</body>
+</html>
