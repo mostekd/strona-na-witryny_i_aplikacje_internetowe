029350f48f4e4372c6b0f03b78b7915d18b65b9f
Dawid Mostowski <dawid-mostowski@o2.pl>
2024-02-29 13:43:57 +0100

    Update dodaj_artykul.php

--- dodaj_artykul.php
+++ dodaj_artykul.php
@@ -12,9 +12,9 @@
             <a href="login.php" class="logout" id="logout"><button>Wyloguj się</button></a>
         </header>
         <form class="MyForm" action="./admin_panel.php" method = "get">
-            <input type=text name="tytul" id="tytul" class="tytul"></input>
-            <input type=text name="tresc" id="tresc" class="tresc"></input>
-            <input type=text name="link" id="link" class="link"></input>
+            <input type=text name="tytul" placeholder="tytuł" id="tytul" class="tytul"></input>
+            <input type=text name="tresc" placeholder="treść" id="tresc" class="tresc"></input>
+            <input type=text name="link" placeholder="link" id="link" class="link"></input>
             <input type="submit"></input>
         </form>
     </body>
