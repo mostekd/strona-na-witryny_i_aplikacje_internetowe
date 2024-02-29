66daf10ab42f873bc163989dab60248d17cf6782
Dawid Mostowski <dawid-mostowski@o2.pl>
2024-02-29 18:41:09 +0100

    Update wpisy.php

--- wpisy.php
+++ wpisy.php
@@ -11,11 +11,17 @@
     }
     $query = 'SELECT `artykul_id`, `title`, `tresc`, `link` FROM `artykul` WHERE 1';
     $data = mysqli_query($connect, $query);
+    
+    $counter = 0; // Licznik artykułów wyświetlonych
     if (mysqli_num_rows($data) > 0)
     {
         while($row = mysqli_fetch_assoc($data))
         {
             echo "<div id='wpis' class='artykul'><a href='artykul.php?id=".$row['artykul_id']."'>".$row['title']."</a><article>".substr($row['tresc'],0,150)." ...</article></div>";
+            $counter++;
+            if ($counter >= 5) {
+                break; // Przerwij pętlę po wyświetleniu 5 artykułów
+            }
         }
     }
     mysqli_close($connect);
