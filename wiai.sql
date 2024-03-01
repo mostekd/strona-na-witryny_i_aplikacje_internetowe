bda7ead7c495e4960dd57239f781010057fa24a9
Dawid Mostowski <dawid-mostowski@o2.pl>
2024-03-01 11:49:00 +0100

    Add files via upload

--- wiai.sql
+++ wiai.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Mar 01, 2024 at 10:28 AM
+-- Generation Time: Mar 01, 2024 at 11:48 AM
 -- Wersja serwera: 10.4.32-MariaDB
 -- Wersja PHP: 8.2.12
 
@@ -51,34 +51,9 @@ CREATE TABLE `artykul` (
   `title` varchar(100) NOT NULL,
   `tresc` mediumtext NOT NULL,
   `link` varchar(100) NOT NULL,
-  `autor` varchar(255) NOT NULL
+  `autor` varchar(255) NOT NULL,
+  `data` datetime NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Dumping data for table `artykul`
---
-
-INSERT INTO `artykul` (`artykul_id`, `title`, `tresc`, `link`, `autor`) VALUES
-(1, 'tytuł1', 'treść1', 'link1', 'autor1'),
-(2, 'tytuł2', 'treść2', 'link2', 'autor2'),
-(3, 'tytuł3', 'treść3', 'link3', 'autor3'),
-(4, 'tytuł4', 'treść4', 'link4', 'autor4'),
-(5, 'tytuł5', 'treść5', 'link5', 'autor5'),
-(6, 'tytuł6', 'treść6', 'link6', 'autor6'),
-(7, 'tytuł7', 'treść7', 'link7', 'autor7'),
-(8, 'tytuł8', 'treść8', 'link8', 'autor8'),
-(9, 'tytuł9', 'treść9', 'link9', 'autor9'),
-(10, 'tytuł10', 'treść10', 'link10', 'autor10'),
-(11, 'tytuł11', 'treść11', 'link11', 'autor11'),
-(12, 'tytuł12', 'treść12', 'link12', 'autor12'),
-(13, 'tytuł13', 'treść13', 'link13', 'autor13'),
-(14, 'tytuł14', 'treść14', 'link14', 'autor14'),
-(15, 'tytuł15', 'treść15', 'link15', 'autor15'),
-(16, 'tytuł16', 'treść16', 'link16', 'autor16'),
-(17, 'tytuł17', 'treść17', 'link17', 'autor17'),
-(18, 'tytuł18', 'treść18', 'link18', 'autor18'),
-(19, 'tytuł19', 'treść19', 'link19', 'autor19'),
-(20, 'tytuł20', 'treść20', 'link20', 'autor20');
 
 -- --------------------------------------------------------
 
@@ -145,7 +120,7 @@ ALTER TABLE `administratorzy`
 -- AUTO_INCREMENT for table `artykul`
 --
 ALTER TABLE `artykul`
-  MODIFY `artykul_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=74;
+  MODIFY `artykul_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=83;
 
 --
 -- AUTO_INCREMENT for table `zdj`
