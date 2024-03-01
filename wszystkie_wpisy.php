c0f8a932e2ffffbe33efcd173eef698b8cce6d22
Dawid Mostowski <dawid-mostowski@o2.pl>
2024-03-01 13:51:02 +0100

    Update wszystkie_wpisy.php

--- wszystkie_wpisy.php
+++ wszystkie_wpisy.php
@@ -27,7 +27,18 @@
     {
         while($row = mysqli_fetch_assoc($data))
         {
-            echo "<div id='wpis' class='artykul'><a href='artykul.php?id=".$row['artykul_id']."'><p>Tytuł:</p>".$row['title']."</a><p>Data dodania:</p>".$row['data']."<article><p>Treść:</p>".substr($row['tresc'],0,150)." ...</article></div>";
+            echo "<div id='wpis' class='artykul'>
+                <a href='artykul.php?id=".$row['artykul_id']."'>
+                    <section class='gorny_panel_wpisu'>
+                        <p>Tytuł:".$row['title']."</p>
+                        <p>Data dodania: ".$row['data']."</p>
+                    </section>
+                    <article>
+                        <p>Treść:</p>
+                        ".substr($row['tresc'],0,150)." ...
+                    </article>
+                </a>
+            </div>";
         }
     }
     mysqli_close($connect);
