c3d651cd9ae95bfaa442440ababcea9c87833076
Dawid Mostowski <dawid-mostowski@o2.pl>
2024-03-02 00:18:04 +0100

    Update ksiazki.php

--- ksiazki.php
+++ ksiazki.php
@@ -8,78 +8,10 @@
 </head>
 <body>
     <div class="header-container">
-        <span id="menu-icon" onclick="openNav()">&#9776; </span>
         <h2>Biblioteka Wesoła Szkoła</h2>
     </div>
-    <div class="zdj_header">
-        <?php
-        // wyświetlanie 3 zdjęć które można podmienić w panelu administracyjnym w zakładce zmień zdjęcie
-        ?>
+    <div class="link">
+        <a href="./index.php"><button>Powrót</button></a>
     </div>
-    <div id="myNav" class="overlay">
-        <a href="javascript:void(0)" class="closebtn" onclick="closeNav()">&times;</a>
-        <div class="overlay-content">
-            <a href="#" onclick="showWpisy()">Strona Główna</a>
-            <a href="#" onclick="showSearchBooks()">Wyszukaj Książki</a>
-            <a href="#" onclick="showSearchGuests()">Księga Gości</a>
-            <a href="#" onclick="showContactInfo()">Kontakt</a>
-        </div>
-        <div class="container">
-            <div class="temp">-°C</div>
-            <div class="summary">----</div>
-            <div class="location"></div>
-        </div>
-        <div id="calendar">
-            <!-- kalendarz z istniejącego skryptu js propozycja: https://air-datepicker.com/ -->
-        </div>
-        <div class="zdjecie">
-        <?php
-                $host = "localhost"; // Host bazy danych
-                $dbname = "wiai"; // Nazwa bazy danych
-                $username = "root"; // Nazwa użytkownika bazy danych
-                $password = ""; // Hasło użytkownika bazy danych
-
-                try {
-                    $con = mysqli_connect($host, $username, $password, $dbname);
-                  
-                }
-                catch (PDOException $e) {
-                    die("Błąd połączenia z bazą danych: " . $e->getMessage());
-                }
-                
-                $select = "SELECT `id`, `nazwa`, `opis`, `path` FROM `zdj` WHERE 1";
-                $data = mysqli_query($con, $select);
-
-                $ilosc = 0;
-                while($row = mysqli_fetch_array($data))
-                {
-                    $ilosc += 1;
-                }
-                $losowe = rand(1,$ilosc);
-                $select = "SELECT `id`, `nazwa`, `opis`, `path` FROM `zdj` WHERE id = $losowe";
-                $data = mysqli_query($con, $select);
-
-                while($row = mysqli_fetch_array($data))
-                {
-                    echo "<img src='{$row[3]}' class='zdj_menu'></img>";
-                }
-            ?>
-        </div>
-    </div>
-    <div class="pokaz_ksiazki">
-    <?php
-    include('db_connection.php');
-    $baza = new db_connection();
-    ?>
-    
-    <!-- formularz z wpisaniem takich pól jak tytuł, autor, rok wydania i po uzupełnieniu części danych: części tytułu będzie można kliknąć przycisk szukaj i wtedy pokażą się wyniki wyszukiwania spełniające kryteria wyszukania (można rozbudować formularz wyszukiwania o własne pomysły)  -->\
-
-    </div>
-    <footer id="footer">
-        Dawid Mostowski 3A
-    </footer>
-    <script src="./js/script.js" defer></script>
-    <script src="./js/pogoda.js"></script>
-    <script src="./js/calendar.js"></script>
 </body>
 </html>
