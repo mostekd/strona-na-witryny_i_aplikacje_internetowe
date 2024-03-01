e6fe18e85aa768ba2ba639b74882d32884a44696
Dawid Mostowski <dawid-mostowski@o2.pl>
2024-03-01 13:42:08 +0100

    update

--- wpisy.php
+++ wpisy.php
@@ -17,7 +17,7 @@
     {
         while($row = mysqli_fetch_assoc($data))
         {
-            echo "<div id='wpis' class='artykul'><a href='artykul.php?id=".$row['artykul_id']."'><p>Tytuł:</p>".$row['title']."</a><p>Data dodania:</p>".$row['data']."<article><p>Treść:</p>".substr($row['tresc'],0,150)." ...</article></div>";
+            echo "<div id='wpis' class='artykul'><a href='artykul.php?id=".$row['artykul_id']."'><section class='gorny_panel_wpisu'><p class='tytul'>Tytuł:".$row['title']."</p><p class='data'>Data dodania: ".$row['data']."</p></section><article><p>Treść:</p>".substr($row['tresc'],0,150)." ...</article></a></div>";
             $counter++;
             if ($counter >= 5) {
                 break; // Przerwij pętlę po wyświetleniu 5 artykułów
