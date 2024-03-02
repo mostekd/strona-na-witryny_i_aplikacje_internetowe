70b54c1e29c2187a1cf5ac10878bbddd83552a9a
Dawid Mostowski <dawid-mostowski@o2.pl>
2024-03-02 23:15:02 +0100

    update

--- dodaj_ksiazki.php
+++ dodaj_ksiazki.php
@@ -12,7 +12,7 @@
         
     </header>
     <a class="przycisk" href="./admin_panel.php">Strona Główna</a>
-    <form class="dodaj_ksiazki_frmularz">
+    <form class="dodaj_ksiazki_frmularz" action="dodaj_ksiazki.php" method = "get">
         <input type=text name="tytul" placeholder="tytuł" id="tytul" class="tytul"></input>
         <input type=text name="autor" placeholder="autor" id="autor" class="autor"></input>
         <input type=text name="wydawnictwo" placeholder="wydawnictwo" id="wydawnictwo" class="wydawnictwo"></input>
@@ -38,9 +38,14 @@
                     $rok_wydania = $_GET['rok_wydania'];
                     $ibsn = $_GET['ibsn'];
                     $aktywna = $_GET['aktywna'];
-                    $baza->insertKsiazka($tytul, $tresc, $link, $autor);
+                    $baza->insertKsiazka($tytul, $wydawnictwo, $rok_wydania, $ibsn, $aktywna);
                 }
             }
+            echo "<div id='ksiazka' class='ksiazka'>Tytuł: ".$row['tytul']."Wydawnictwo: ".$row['$wydawnictwo']."Rok wydania: ".$row['$rok_wydania']."ibsn: ".$row['$ibsn']."Aktywna: ".$row['$aktywna']."
+                <button class='delete'><a href=dodaj_ksiazki.php?del=True&id=".$row['id_ksiazki'].">
+                Usuń wpis
+                </a></button>
+                </div>";
         ?>
     </div>
     <!-- Administrator będzie miał również możliwość wprowadzania nowych książek, jak również ich modyfikację i usuwanie, tabela książki powinna zawierać (id, tytuł, autor, wydawnictwo, rok wydania, isbn, aktywna(bool), uwagi), -->
