c1d69e00b67d00c0e16827a2c0d436d786c1754d
Dawid Mostowski <dawid-mostowski@o2.pl>
2024-03-02 10:30:55 +0100

    Update contact_info.php

--- contact_info.php
+++ contact_info.php
@@ -4,6 +4,19 @@
     <p>Adres: ul. Szkolna 1 , 54-230 Gdańsk</p>
     <p>Telefon: 123-456-789</p>
     <p>Email: biblioteka@wesolaszkola.pl</p>
+
+    <form class="formularz_kontaktowy">
+        <label for="inquiryType">Typ zapytania:</label>
+        <select id="inquiryType" name="inquiryType">
+            <option value="null">--</option>
+            <option value="Dostępność książki">Zapytanie o dostępność książki</option>
+            <option value="Rezerwacja">Prośba o rezerwację</option>
+            <option value="Inna sprawa">Inna sprawa</option>
+        </select>
+        <input type=text name="imie" placeholder="imię" id="imie" class="imie"></input>
+        <input type=text name="nazwisko" placeholder="nazwisko" id="nazwisko" class="nazwisko"></input>
+        <textarea type=text name="wiadomosc" placeholder="treść wiadomości" id="wiadomosc" class="wiadomosc"></textarea>
+    </form>
 </div>
 
 <!-- strona zawiera formularz kontaktowy zdolny wysłać wiadomość e-mail na e-mail podany w punkcie nr 1 w opisie zadania (na potrzeby sprawdzenia działania funkcjonalności można zamienić na dawid-mostowski@o2.pl). 
