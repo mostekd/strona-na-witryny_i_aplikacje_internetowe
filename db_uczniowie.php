e7c9f8aed8843859e15e0cbabaab72dc07a3d3e5
Brujah1685r <107842376+Brujah1685r@users.noreply.github.com>
2024-03-05 15:12:57 +0100

    Update db_uczniowie.php

--- db_uczniowie.php
+++ db_uczniowie.php
@@ -4,8 +4,8 @@ class db_uczniowie extends db_connection{
     function insertUczen($imie, $nazwisko, $PESEL, $email, $uwagi){
         $query = "INSERT INTO `uczen`(`imie`, `nazwisko`, `PESEL`, `email`, `uwagi`) VALUES ('".$imie."','".$nazwisko."','".$PESEL."','".$email."','".$uwagi."');";
         $data = mysqli_query($this->connect, $query);
-        if (mysqli_num_rows($data) > 0){
-            return $data;
+        if ($data == TRUE){
+            $mysqli->commit();
         }
     }
 
