a56a657c9a51c4b3348724f436f858cc60395349
Dawid Mostowski <dawid-mostowski@o2.pl>
2024-03-05 12:59:19 +0100

    update

--- uczniowie.php
+++ uczniowie.php
@@ -13,12 +13,14 @@
     </header>
     <a class="przycisk" href="./admin_panel.php">Strona Główna</a>
     <main>
+        <br>Dodaj ucznia:
         <form>
-        <input type=text name="imie" placeholder="imię" id="imie" class="imie"></input>
-        <input type=text name="nazwisko" placeholder="nazwisko" id="nazwisko" class="nazwisko"></input>
-        <input type=text name="PESEL" placeholder="PESEL" id="PESEL" class="PESEL"></input>
-        <input type=email name="email" placeholder="e-mail" id="email" class="email"></input>
-        <textarea type=text name="uwagi" placeholder="uwagi" id="uwagi" class="uwagi"></textarea>
+            <input type=text name="imie" placeholder="imię" id="imie" class="imie"></input>
+            <input type=text name="nazwisko" placeholder="nazwisko" id="nazwisko" class="nazwisko"></input>
+            <input type=text name="PESEL" placeholder="PESEL" id="PESEL" class="PESEL"></input>
+            <input type=email name="email" placeholder="e-mail" id="email" class="email"></input>
+            <textarea type=text name="uwagi" placeholder="uwagi" id="uwagi" class="uwagi"></textarea>
+            <input type="submit"></input>
         </form>
     </main>
 <!-- Formularz dodający ucznia do bazy danych, pole wyszukiwania uczniów, raport (tabela) po kliknięciu na przycisk będzie pokazywała wszystkich uczniów lub uczniów zaczynających się na określoną literę nazwiska lub imienia (wyszukiwanie ucznia) wraz z informacją jakie książki wypożyczył do tej pory i jakie książki ma wypożyczone, -->
