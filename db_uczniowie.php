707e2528fe1fb53ce4623ca9de35dac108d49c6e
Dawid Mostowski <dawid-mostowski@o2.pl>
2024-03-05 13:45:30 +0100

    update

--- db_uczniowie.php
+++ db_uczniowie.php
@@ -3,7 +3,7 @@ include('db_connection.php');
 class db_ksiazki extends db_connection{
 
     function insertUczen($imie, $nazwisko, $PESEL, $email, $uwagi){
-        $query = 'INSERT INTO `uczen`(`imie`, `nazwisko`, `PESEL`, `email`, `uwagi`) VALUES ('[value-2]','[value-3]','[value-4]','[value-5]','[value-6]';';
+        $query = 'INSERT INTO `uczen`(`imie`, `nazwisko`, `PESEL`, `email`, `uwagi`) VALUES ('$imie','$nazwisko','$PESEL','$email','$uwagi';';
         $data = mysqli_query($this->connect, $query);
         if (mysqli_num_rows($data) > 0){
             return $data;
