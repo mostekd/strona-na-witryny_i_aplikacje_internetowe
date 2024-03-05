6d77e0d0203bedda25d7c7fe4feee3ba7b05f063
Dawid Mostowski <dawid-mostowski@o2.pl>
2024-03-05 12:45:16 +0100

    update

--- wypozyczenia.php
+++ wypozyczenia.php
@@ -13,7 +13,7 @@
     </header>
     <a class="przycisk" href="./admin_panel.php">Strona Główna</a>
 
-<!-- tabela wypożyczenia powinna zawierać (id, id książki, id ucznia wypożyczającego, data wypożyczenia, data zwrotu), Administrator będzie mógł również po kliknięciu w przycisk w osobnym raporcie (tabeli) zobaczyć wszystkie książki przetrzymywane powyżej określonego okresu (okres wypożyczenia książki będzie możliwy do zmiany przez Administratora w osobnym miejscu, domyślnie 2 tygodnie),  -->
+<!-- tabela wypożyczenia powinna zawierać (id, id książki, id ucznia wypożyczającego, data wypożyczenia, data zwrotu), Administrator będzie mógł również po kliknięciu w przycisk w osobnym raporcie (tabeli) zobaczyć wszystkie książki przetrzymywane powyżej określonego okresu (okres wypożyczenia książki będzie możliwy do zmiany przez Administratora w osobnym miejscu, domyślnie 2 tygodnie), Administrator przy wyporzyczeniu książki przez ucznia wybiera go z listy uczniów z tabeli uczniowie -->
 
 </body>
 </html>
