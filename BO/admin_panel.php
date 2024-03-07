093b01e568ec61b662a681ea2f83c48b560a121b
Dawid Mostowski <dawid-mostowski@o2.pl>
2024-03-07 22:12:57 +0100

    update

--- BO/admin_panel.php
+++ BO/admin_panel.php
@@ -11,12 +11,12 @@
         <h2>Biblioteka Wesoła Szkoła<br>Panel Administracyjny</h2>
         <a href="login.php" class="logout" id="logout">Wyloguj się</a>
     </header>
-    <a class="przycisk" href="./zatwierdz_goscia.php">Zatwierdzenie wpisów użytkowników</a>
-    <a class="przycisk" href="./lista_ksiazki.php">Ksiązki</a>
-    <a class="przycisk" href="./zmien_zdjecie.php">Zmień zdjęcie</a>
     <a class="przycisk" href="./lista_wpisy.php">Wpisy</a>
-    <a class="przycisk" href="./wypozyczenia.php">Wypożyczenia</a>
+    <a class="przycisk" href="./lista_ksiazki.php">Ksiązki</a>
     <a class="przycisk" href="./lista_uczniowie.php">Uczniowie</a>
+    <a class="przycisk" href="./wypozyczenia.php">Wypożyczenia</a>
+    <a class="przycisk" href="./zatwierdz_goscia.php">Zatwierdzenie wpisów użytkowników</a>
+    <a class="przycisk" href="./zmien_zdjecie.php">Zmień zdjęcie</a>
     </div>
 </body>
 </html>
--- BO/edytuj_ucznia.php
+++ BO/edytuj_ucznia.php
@@ -11,7 +11,7 @@
             <h2>Biblioteka Wesoła Szkoła<br>Panel Administracyjny</h2>
             <a href='login.php' class='logout' id='logout'>Wyloguj się</a>
         </header>
-        <a class="przycisk" href="./lista_uczniowie.php">Książki</a>
+        <a class="przycisk" href="./lista_uczniowie.php">Uczniowie</a>
 
         <?php
          include('../DB/db_uczniowie.php');
@@ -19,18 +19,18 @@
          
             if(!empty($_GET)){                
                 $baza->databaseConnect();
-                $id_ksiazki=$_GET['id'];
-                $data = $baza->selectKsiazkaByID($id_ksiazki);
+                $id_ucznia=$_GET['id'];
+                $data = $baza->selectUczenByID($id_ucznia);
                 if (!empty($data)){
                     while($row = mysqli_fetch_assoc($data))
                     {
                         echo "<form class='MyForm' action='./lista_uczniowie.php' method = 'get'>";
                         echo "<input type=text name='imie' placeholder='imię' id='imie' class='imie' value=".$row['imie']."></input>";
-                        echo "<textarea type=text name='nazwisko' placeholder='nazwisko' id='nazwisko' class='nazwisko'>".$row['nazwisko']."</textarea>";
+                        echo "<input type=text name='nazwisko' placeholder='nazwisko' id='nazwisko' class='nazwisko'".$row['nazwisko']."></input>";
                         echo "<input type=text  name='PESEL' placeholder='PESEL' id='PESEL' class='PESEL' value=".$row['PESEL']."></input>";
                         echo "<input type=text name='email' placeholder='email' id='email' class='email' value=".$row['email']."></input>";
                         echo "<textarea type=text name='uwagi' placeholder='uwagi' id='uwagi' class='uwagi' value=".$row['uwagi']."></textarea>";
-                        echo "<input type=hidden name='id_ksiazki' id='id_ksiazki' class='id_ksiazki' value=".$row['id_ksiazki']."></input>";
+                        echo "<input type=hidden name='id_ucznia' id='id_ucznia' class='id_ucznia' value=".$row['id_ucznia']."></input>";
                         echo "<input type=hidden name='opcja' id='opcja' class='opcja' value='edytuj'></input>";
                         echo "<input type='submit'></input>";
                         echo "</form>";
