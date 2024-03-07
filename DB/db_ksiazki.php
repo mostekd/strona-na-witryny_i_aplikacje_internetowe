b4fce3013e216a6aa82d73b35336e1dcd39b1fe9
Dawid Mostowski <dawid-mostowski@o2.pl>
2024-03-07 22:01:28 +0100

    update

--- DB/db_ksiazki.php
+++ DB/db_ksiazki.php
@@ -28,5 +28,14 @@ class db_ksiazki extends db_connection{
         $query = "UPDATE `ksiazki` SET `tytul`='".$tytul."',`autor`='".$autor."',`wydawnictwo`='".$wydawnictwo."',`rok_wydania`='".$rok_wydania."',`isbn`='".$isbn."',`aktywna`='".$aktywna."',`uwagi`='".$uwagi.";";
     }
 
+    function selectKsiazkaByID($id_ksiazki){
+        $query = "SELECT `id_ksiazki`, `tytul`, `autor`, `wydawnictwo`, `rok_wydania`, `isbn`, `aktywna`, `uwagi` FROM `ksiazki` WHERE id_ksiazki =".$id_ksiazki;
+		$data = mysqli_query($this->connect, $query);
+		
+		if (mysqli_num_rows($data) > 0) {
+            return $data;
+        }
+    }
+
 }
 ?>
