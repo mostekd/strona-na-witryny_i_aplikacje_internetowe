32bc4ff06b0138f7604fdeb7fd6915e55e5f67a3
Dawid Mostowski <dawid-mostowski@o2.pl>
2024-03-07 20:48:11 +0100

    update

--- BO/dodaj_ucznia.php
+++ BO/dodaj_ucznia.php
@@ -1,48 +1,27 @@
 <!DOCTYPE html>
-<html lang="pl">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Panel Administracyjny</title>    
-    <link rel="stylesheet" href="admin_panel.css">
-</head>
-<body>
-    <header>
-        <h2>Biblioteka Wesoła Szkoła<br>Panel Administracyjny</h2>
-        
-    </header>
-    <a class="przycisk" href="./admin_panel.php">Strona Główna</a>
-    <main>
-        <br>Dodaj ucznia:
-        <form>
-            <input type=text name="imie" placeholder="imię" id="imie" class="imie"></input>
-            <input type=text name="nazwisko" placeholder="nazwisko" id="nazwisko" class="nazwisko"></input>
-            <input type=text name="PESEL" placeholder="PESEL" id="PESEL" class="PESEL"></input>
-            <input type=email name="email" placeholder="e-mail" id="email" class="email"></input>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Panel Administracyjny</title>    
+        <link rel="stylesheet" href="admin_panel.css">
+    </head>
+    <body>
+        <header>
+            <h2>Biblioteka Wesoła Szkoła<br>Panel Administracyjny</h2>
+            <a href="login.php" class="logout" id="logout">Wyloguj się</a>
+        </header>
+        <a class="przycisk" href="./lista_ksiazki.php">Książki</a>
+        <form class="MyForm" action="./lista_ksiazki.php" method = "get">
+            <input type=text name="tytul" placeholder="tytuł" id="tytul" class="tytul"></input>
+            <input type=text name="autor" placeholder="autor" id="autor" class="autor"></input>
+            <input type=text name="wydawnictwo" placeholder="wydawnictwo" id="wydawnictwo" class="wydawnictwo"></input>
+            <input type=text name="rok_wydania" placeholder="rok_wydania" id="rok_wydania" class="rok_wydania"></input>
+            <input type=text  name="isbn" placeholder="isbn" id="isbn" class="isbn"></input>
             <textarea type=text name="uwagi" placeholder="uwagi" id="uwagi" class="uwagi"></textarea>
+            Aktywna: <input type=checkbox name="aktywna" id="aktywna" class="aktywna"></input>
+            <input type=hidden name="opcja" id="opcja" class="opcja" value='dodaj'></input>
             <input type="submit"></input>
         </form>
-    </main>
-
-    <?php
-    include('../DB/db_uczniowie.php');
-    $baza = new db_uczniowie();
-
-    if(!empty($_GET)){
-        $baza->databaseConnect();
-        if(isset($_GET['del']))
-        {
-            $id_ucznia=$_GET['id'];
-            $baza->deleteUczen($id_ucznia);
-        }
-        elseif(isset($_GET['imie'])){
-            $imie = $_GET['imie'];
-            $nazwisko = $_GET['nazwisko'];
-            $PESEL = $_GET['PESEL'];
-            $email = $_GET['email'];
-            $uwagi = $_GET['uwagi'];
-            $baza->insertUczen($imie, $nazwisko, $PESEL, $email, $uwagi);
-        }
-    }
-    $baza->databaseConnect();
-?>
+    </body>
+</html>
---
+++ BO/edytuj_ucznia.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang='en'>
+    <head>
+        <meta charset='UTF-8'>
+        <meta name='viewport' content='width=device-width, initial-scale=1.0'>
+        <title>Panel Administracyjny</title>    
+        <link rel='stylesheet' href='admin_panel.css'>
+    </head>
+    <body>
+        <header>
+            <h2>Biblioteka Wesoła Szkoła<br>Panel Administracyjny</h2>
+            <a href='login.php' class='logout' id='logout'>Wyloguj się</a>
+        </header>
+        <a class="przycisk" href="./lista_ksiazki.php">Książki</a>
+
+        <?php
+         include('../DB/db_artykuly.php');
+         $baza = new db_artykuly();
+         
+            if(!empty($_GET)){                
+                $baza->databaseConnect();
+                $artykul_id=$_GET['id'];
+                $data = $baza->selectArtykulByID($artykul_id);
+                if (!empty($data)){
+                    while($row = mysqli_fetch_assoc($data))
+                    {
+                        echo "<form class='MyForm' action='./lista_ksiazki.php' method = 'get'>";
+                        echo "<input type=text name='tytul' placeholder='tytuł' id='tytul' class='tytul' value=".$row['title']."></input>";
+                        echo "<textarea type=text name='tresc' placeholder='treść' id='tresc' class='tresc'>".$row['tresc']."</textarea>";
+                        echo "<input type=url  name='link' placeholder='link' id='link' class='link' value=".$row['link']."></input>";
+                        echo "<input type=text name='autor' placeholder='autor' id='autor' class='autor' value=".$row['autor']."></input>";
+                        echo "<input type=hidden name='artykul_id' id='artykul_id' class='artykul_id' value=".$row['artykul_id']."></input>";
+                        echo "<input type=hidden name='opcja' id='opcja' class='opcja' value='edytuj'></input>";
+                        echo "<input type='submit'></input>";
+                        echo "</form>";
+                    }
+                }
+            }
+        ?>
+
+        
+    </body>
+</html>
