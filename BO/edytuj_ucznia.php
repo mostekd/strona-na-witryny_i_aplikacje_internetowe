baacfb6440c7c519dc8b0169ba6725d712023a22
Brujah1685r <107842376+Brujah1685r@users.noreply.github.com>
2024-03-09 19:55:59 +0100

    Update edytuj_ucznia.php

--- BO/edytuj_ucznia.php
+++ BO/edytuj_ucznia.php
@@ -26,10 +26,10 @@
                     {
                         echo "<form class='MyForm' action='./lista_uczniowie.php' method = 'get'>
                         <input type=text name='imie' placeholder='imię' id='imie' class='imie' value=".$row['imie']."></input>
-                        <input type=text name='nazwisko' placeholder='nazwisko' id='nazwisko' class='nazwisko'".$row['nazwisko']."></input>
+                        <input type=text name='nazwisko' placeholder='nazwisko' id='nazwisko' class='nazwisko' value=".$row['nazwisko']."></input>
                         <input type=text  name='PESEL' placeholder='PESEL' id='PESEL' class='PESEL' value=".$row['PESEL']."></input>
                         <input type=text name='email' placeholder='email' id='email' class='email' value=".$row['email']."></input>
-                        <textarea type=text name='uwagi' placeholder='uwagi' id='uwagi' class='uwagi' value=".$row['uwagi']."></textarea>
+                        <textarea type=text name='uwagi' placeholder='uwagi' id='uwagi' class='uwagi'>".$row['uwagi']."</textarea>
                         <input type=hidden name='id_ucznia' id='id_ucznia' class='id_ucznia' value=".$row['id_ucznia']."></input>
                         <input type=hidden name='opcja' id='opcja' class='opcja' value='edytuj'></input><input type='submit'></input>
                         </form>";
