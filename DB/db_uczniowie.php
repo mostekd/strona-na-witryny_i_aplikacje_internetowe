3dc84928bb4d162ed74034ffa5857c29ef1ae2c7
Brujah1685r <107842376+Brujah1685r@users.noreply.github.com>
2024-03-09 19:27:07 +0100

    Update db_uczniowie.php

--- DB/db_uczniowie.php
+++ DB/db_uczniowie.php
@@ -1,9 +1,8 @@
 <?php
 include('db_connection.php');
 class db_uczniowie extends db_connection{
-    
     function selectUczen(){
-        $query = 'SELECT * FROM `uczen` WHERE 1';
+        $query = 'SELECT `id_uczen`,`imie`, `nazwisko`, `PESEL`, `email`, `uwagi` FROM `uczen` WHERE 1';
         $data = mysqli_query($this->connect, $query);
         if (mysqli_num_rows($data) > 0){
             return $data;
@@ -17,29 +16,29 @@ class db_uczniowie extends db_connection{
     }
 
     function deleteUczen($id_ucznia){
-            $query = "Delete from uczen where id_ucznia =".$id_ucznia.";";
-            $data = mysqli_query($this->connect, $query);
-            unset($_GET['id']);
-            header('location: ./lista_uczniowie.php');   
-            $this->close();
+	$query = "Delete from uczen where id_ucznia =".$id_ucznia.";";
+	$data = mysqli_query($this->connect, $query);
+	unset($_GET['id']);
+	header('location: ./lista_uczniowie.php');   
+	$this->close();
     }
 
     function updateUczen($id_ucznia, $imie, $nazwisko, $PESEL, $email, $uwagi){
         $query = "UPDATE `uczen` SET `imie`='".$imie."',`nazwisko`='".$nazwisko."',`PESEL`='".$PESEL."',`email`='".$email."',`uwagi`='".$uwagi."' WHERE `id_ucznia`=".$id_ucznia.";";
 		$data = mysqli_query($this->connect, $query);
 		
-		unset($_GET['id']);
+	unset($_GET['id']);
         header('location: ../BO/lista_uczniowie.php');   
         $this->close();
     }
 
     function selectUczenByID($id_ucznia){
-        $query = "SELECT * FROM `uczen` WHERE id_ucznia =".$id_ucznia;
-		$data = mysqli_query($this->connect, $query);
-        
-		if (mysqli_num_rows($data) > 0) {
-            return $data;
-        }
+        $query = "SELECT `id_uczen`,`imie`, `nazwisko`, `PESEL`, `email`, `uwagi` FROM `uczen` WHERE id_ucznia =".$id_ucznia;
+	$data = mysqli_query($this->connect, $query);
+
+	if (mysqli_num_rows($data) > 0) {
+		return $data;
+	}
     }
     }
 ?>
