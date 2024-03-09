1abc0d096897ed6914b10f271ac5359893d19517
Brujah1685r <107842376+Brujah1685r@users.noreply.github.com>
2024-03-09 19:43:46 +0100

    Update edytuj_ksiazke.php

--- BO/edytuj_ksiazke.php
+++ BO/edytuj_ksiazke.php
@@ -30,7 +30,7 @@
                         echo "<input type=text name='wydawnictwo' placeholder='wydawnictwo' id='wydawnictwo' class='wydawnictwo' value=".$row['wydawnictwo']."></input>";
                         echo "<input type=text name='rok_wydania' placeholder='rok wydania' id='rok_wydania' class='rok_wydania' value=".$row['rok_wydania']."></input>";
                         echo "<input type=text name='isbn' placeholder='isbn' id='isbn' class='isbn' value=".$row['isbn']."></input>";
-                        echo "<textarea type=text name='uwagi' placeholder='uwagi' id='uwagi' class='uwagi' value=".$row['uwagi']."></textarea>";
+                        echo "<textarea type=text name='uwagi' placeholder='uwagi' id='uwagi' class='uwagi'>".$row['uwagi']."</textarea>";
                         echo "Aktywna: <input type=checkbox name='aktywna' placeholder='aktywna' id='aktywna' class='aktywna' value=".$row['aktywna']."></input>";
                         echo "<input type=hidden name='id_ksiazki' id='id_ksiazki' class='id_ksiazki' value=".$row['id_ksiazki']."></input>";
                         echo "<input type=hidden name='opcja' id='opcja' class='opcja' value='edytuj'></input>";
