0497da73ba85e40fef11f0ee04a3a465dd7b3f22
Dawid Mostowski <dawid-mostowski@o2.pl>
2024-03-10 22:36:16 +0100

    update

--- BO/admin_panel.php
+++ BO/admin_panel.php
@@ -6,6 +6,7 @@
     <title>Panel Administracyjny</title>    
     <link rel="stylesheet" href="admin_panel.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <script src="https://kit.fontawesome.com/1deffa5961.js" crossorigin="anonymous"></script>
 </head>
 <body>
     <header>
@@ -13,12 +14,12 @@
         <a href="login.php" class="logout" id="logout">Wyloguj się</a>
     </header>
     <div class="panel_prawy">
-    <i class="fa-solid fa-book"></i><a class="przycisk" href="./lista_wpisy.php">Wpisy</a>
-    <a class="przycisk" href="./lista_ksiazki.php">Ksiązki</a>
-    <a class="przycisk" href="./lista_uczniowie.php">Uczniowie</a>
-    <a class="przycisk" href="./wypozyczenia.php">Wypożyczenia</a>
-    <a class="przycisk" href="./zatwierdz_goscia.php">Zatwierdzenie wpisów użytkowników</a>
-    <a class="przycisk" href="./zmien_zdjecie.php">Zmień zdjęcie</a>
+    <a class="przycisk" href="./lista_wpisy.php"><i class="fa-regular fa-newspaper" style="color: #005eff;"></i> Wpisy</a>
+    <a class="przycisk" href="./lista_ksiazki.php"><i class="fa-solid fa-book" style="color: #005eff;"></i> Ksiązki</a>
+    <a class="przycisk" href="./lista_uczniowie.php"><i class="fa-regular fa-user" style="color: #005eff;"></i> Uczniowie</a>
+    <a class="przycisk" href="./wypozyczenia.php"><i class="fa-solid fa-book-bookmark" style="color: #005eff;"></i> Wypożyczenia</a>
+    <a class="przycisk" href="./zatwierdz_goscia.php"><i class="fa-solid fa-user-check" style="color: #005eff;"></i> Zatwierdzenie wpisów użytkowników</a>
+    <a class="przycisk" href="./zmien_zdjecie.php"><i class="fa-regular fa-image" style="color: #005eff;"></i> Zmień zdjęcie</a>
     </div>
 </body>
 </html>
