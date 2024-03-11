06ca0a4f26f0f7f3140f51710656102dec4db0b8
Dawid Mostowski <dawid-mostowski@o2.pl>
2024-03-11 09:31:44 +0100

    update

--- BO/dodaj_ksiazka.php
+++ BO/dodaj_ksiazka.php
@@ -5,13 +5,17 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>Panel Administracyjny</title>    
         <link rel="stylesheet" href="admin_panel.css">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+        <script src="https://kit.fontawesome.com/1deffa5961.js" crossorigin="anonymous"></script>
     </head>
     <body>
         <header>
             <h2>Biblioteka Wesoła Szkoła<br>Panel Administracyjny</h2>
             <a href="login.php" class="logout" id="logout">Wyloguj się</a>
         </header>
-        <a class="przycisk" href="./lista_ksiazki.php">Książki</a>
+        <div class="panel_prawy">
+        <a class="przycisk" href="./lista_ksiazki.php"><i class="fa-solid fa-book" style="color: #fff;"></i> Ksiązki</a>
+        </div>
         <form class="MyForm" action="./lista_ksiazki.php" method = "get">
             <input type=text name="tytul" placeholder="tytuł" id="tytul" class="tytul"></input>
             <input type=text name="autor" placeholder="autor" id="autor" class="autor"></input>
--- BO/lista_ksiazki.php
+++ BO/lista_ksiazki.php
@@ -5,14 +5,18 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Panel Administracyjny</title>    
     <link rel="stylesheet" href="admin_panel.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <script src="https://kit.fontawesome.com/1deffa5961.js" crossorigin="anonymous"></script>
 </head>
 <body>
     <header>
         <h2>Biblioteka Wesoła Szkoła<br>Panel Administracyjny</h2>
         <a href="login.php" class="logout" id="logout">Wyloguj się</a>
     </header>
-    <a class="przycisk" href="./admin_panel.php">Strona główna</a>
+    <div class="panel_prawy">
+    <a class="przycisk" href="./admin_panel.php"><i class="fa-solid fa-house" style="color: #fff;"></i>Strona główna</a>
     <a class="przycisk" href="./dodaj_ksiazka.php">Dodaj książkę</a>
+    </div>
 
     <?php
     include('../DB/db_ksiazki.php');
