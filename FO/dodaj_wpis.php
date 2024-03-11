45db8818455c2afcd84fafe332c56b8ebbe314c4
Dawid Mostowski <dawid-mostowski@o2.pl>
2024-03-11 22:04:14 +0100

    update

--- FO/dodaj_wpis.php
+++ FO/dodaj_wpis.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Biblioteka</title>
-    <link rel="stylesheet" href="./FO/style.css">
+    <link rel="stylesheet" href="./style.css">
 </head>
     <body>
         <?php
