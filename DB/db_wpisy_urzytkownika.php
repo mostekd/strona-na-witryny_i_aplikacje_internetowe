51cdc7d5abe3a6a9754a1667483e917e022d127b
Dawid Mostowski <dawid-mostowski@o2.pl>
2024-03-11 21:29:23 +0100

    update

---
+++ DB/db_wpisy_urzytkownika.php
@@ -0,0 +1,41 @@
+<?php
+include('db_connection.php');
+class db_wpisy_urzytkownika extends db_connection{
+    
+    function deleteWpisUrzytkownika($id_wpisu_urzytkownika){
+        $query = "Delete from artykul where id_wpisu_urzytkownika =".$id_wpisu_urzytkownika.";";
+        $data = mysqli_query($this->connect, $query);
+        unset($_GET['id']);
+        header('location: ../BO/lista_wpisy.php');   
+        $this->close();
+    }
+
+    function selectWpisUrzytkownika(){
+        $query = 'SELECT * FROM `wpisy_urzytkownika` ORDER BY id_wpisu_urzytkownika DESC';
+        $data = mysqli_query($this->connect, $query);
+        if (mysqli_num_rows($data) > 0){
+            return $data;
+        }
+    }
+
+    function insertWpisUrzytkownika($tytul, $tresc, $autor, $data, $aktywny){
+        $dateTest = date('Y-m-d H:i:s');
+        $query = "INSERT INTO `wpisy_urzytkownika`(`tytul`, `tresc`, `autor`, `data`, `aktywny`) VALUES ('".$tytul."','".$tresc."','".$autor."','".$data."','".$aktywny."');";
+        $data = mysqli_query($this->connect, $query);
+        header('location: ../BO/lista_wpisy.php'); 
+        $this->close();
+    }
+
+    function updateWpisUrzytkownika($id_wpisu_urzytkownika, $tytul, $tresc, $autor, $aktywny){
+        $query = "UPDATE `wpisy_urzytkownika` SET `title`='".$tytul."',`tresc`='".$tresc."',`autor`='".$autor."',`aktywny'".$aktywny."' WHERE `id_wpisu_urzytkownika`=".$id_wpisu_urzytkownika.";";
+		$data = mysqli_query($this->connect, $query);
+		
+		unset($_GET['id']);
+        header('location: ../BO/lista_wpisy_goscia.php');   
+        $this->close();
+    }
+}
+?>
+
+}
+?>
