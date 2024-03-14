fc2e8597bec91fce5cffa251d1c5bdd90828d551
Dawid Mostowski <dawid-mostowski@o2.pl>
2024-03-14 12:53:01 +0100

    update

--- BO/lista_wpisy.php
+++ BO/lista_wpisy.php
@@ -1,14 +1,6 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Panel Administracyjny</title>    
-    <link rel="stylesheet" href="admin_panel.css">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-    <script src="https://kit.fontawesome.com/1deffa5961.js" crossorigin="anonymous"></script>
-</head>
-<body>
+<?php
+include('./head_admin.php');
+?>
     <header>
         <h2>Biblioteka Wesoła Szkoła<br>Panel Administracyjny</h2>
         <a href="login.php" class="logout" id="logout"><i class="fa-solid fa-arrow-right-from-bracket" style="color: #fff;"></i> Wyloguj się</a>
