270b1f5d7837433e9a13c04191496011675e06cf
Dawid Mostowski <dawid-mostowski@o2.pl>
2024-03-22 11:14:32 +0100

    update

--- FO/rightNav.php
+++
@@ -1,7 +0,0 @@
-<div id="myNav" class="panel_prawy">
-    <div class="">
-        <script src="../js/pogoda.js"></script>
-        <script src="../js/calendar.js"></script>
-        <p>test</p>
-    </div>
-</div>
