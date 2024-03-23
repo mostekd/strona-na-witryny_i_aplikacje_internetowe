bb315e46eda7db785fe7e120cfade5c7668a119b
Dawid Mostowski <dawid-mostowski@o2.pl>
2024-03-23 22:34:04 +0100

    update

--- BO/book_list.php
+++ BO/book_list.php
@@ -60,11 +60,11 @@ include('./head_admin.php');
         <?php
             while($row = mysqli_fetch_assoc($data))
             {
-                echo "<div id='wpis' class='artykul'>Tytuł: ".$row['title']." author: ".$row['author']." publisher: ".$row['publisher']." Rok wydania: ".$row['publishYear']." ISBN: ".$row['isbn']." active: ".$row['active']." comment: ".$row['comment']."
-                <button class='delete'><a href=book_list.php?del=True&id=".$row['idbook'].">
+                echo "<div id='wpis' class='artykul'>Tytuł: ".$row['title']." Autor: ".$row['author']." publisher: ".$row['publisher']." Rok wydania: ".$row['publishYear']." ISBN: ".$row['isbn']." active: ".$row['active']." comment: ".$row['comments']."
+                <button class='delete'><a href=book_list.php?del=True&id=".$row['id_book'].">
                 Usuń książke
                 </a></button>
-                <button class='delete'><a href=book_edit.php?id=".$row['idbook'].">
+                <button class='delete'><a href=book_edit.php?id=".$row['id_book'].">
                 Edytuj książke
                 </a></button>
                 </div>";
