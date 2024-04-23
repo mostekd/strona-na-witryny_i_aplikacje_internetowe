acbe9e4f97478ce51575fa25e6cd463d84056a02
Dawid Mostowski <dawid-mostowski@o2.pl>
2024-04-23 11:32:39 +0200

    update

--- DB/db_zdj.php
+++ DB/db_zdj.php
@@ -1,8 +1,16 @@
 <?php
 include('db_connection.php');
 class db_zdj extends db_connection{
-    function selectUczen(){
+    function selectRandomZdj(){
         $query = 'SELECT * FROM `zdj` WHERE';
+        $data = mysqli_query($this->connect, $query);
+        if (mysqli_num_rows($data) > 0){
+		return $data;
+        }
+    }
+
+    function selectZdj(){
+        $query = 'SELECT * FROM `zdj` WHERE 1';
         $data = mysqli_query($this->connect, $query);
         if (mysqli_num_rows($data) > 0){
 		return $data;
