39ee0c1c41f4594490226740928c357cd13dc420
Dawid Mostowski <dawid-mostowski@o2.pl>
2024-04-23 12:02:35 +0200

    update

--- FO/rightNav.php
+++ FO/rightNav.php
@@ -1,5 +1,8 @@
 <div id="myNav" class="panel_prawy">
     <div class="">
+        <?
+        $data = $baza->selectMaxZdjId();
+        ?>
         <img src="#" alt="zdj_panel-prawy">
         <script src="../js/pogoda.js"></script>
         <script src="../js/calendar.js"></script>
