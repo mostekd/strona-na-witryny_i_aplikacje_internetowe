e8cb14630ffecc1d1d5a76baaa5b250004d46a5b
Dawid Mostowski <dawid-mostowski@o2.pl>
2024-04-29 12:39:14 +0200

    update

--- DB/db_zdj.php
+++ DB/db_zdj.php
@@ -1,15 +1,11 @@
 <?php
 class db_zdj extends db_connection{
-    function insertZdj($path) {
-        $query = "INSERT INTO zdj (path) VALUES ('$path')";
-        return mysqli_query($this->connect, $query);
-    }
-
     function selectMaxZdjId() {
-        $query = "SELECT MAX(id) AS mmax FROM zdj";
+        $query = "SELECT MAX(id) as mmax FROM `zdj`";
         $data = mysqli_query($this->connect, $query);
         if (mysqli_num_rows($data) > 0) {
-            while($row = mysqli_fetch_assoc($data)) {
+            while($row = mysqli_fetch_assoc($data))
+            {
                 return $row['mmax'];
             }
         } else {
@@ -20,10 +16,10 @@ class db_zdj extends db_connection{
     function selectRandomZdj() {
         $max_id = $this->selectMaxZdjId();
         $random_id = rand(1, $max_id);
-        $query = "SELECT path FROM zdj WHERE id = $random_id";
+        $query = "SELECT `path` FROM `zdj` WHERE id = $random_id";
         $data = mysqli_query($this->connect, $query);
         if ($data && mysqli_num_rows($data) > 0) {
-            return mysqli_fetch_assoc($data)['path'];
+            return mysqli_fetch_assoc($data);
         } else {
             return false;
         }
